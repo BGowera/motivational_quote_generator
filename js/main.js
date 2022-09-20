8644c9fdebad1855592bc58bd5e910cb1ecab44f
BGowera <91614476+BGowera@users.noreply.github.com>
2022-09-20 07:36:00 +0200

    basic get functionality

--- js/main.js
+++ js/main.js
@@ -4,8 +4,14 @@ const form = document.getElementById('form');
 form.addEventListener('submit',async function (e) { 
         e.preventDefault();
         const req = await axios.get("https://type.fit/api/quotes");
-        console.log(req.data[0]);
+      
+const randCall = Math.floor(Math.random() * 10) ;
+        const quote = `${req.data[randCall].text}, - ${req.data[randCall].author}`;
+        const list = document.createElement('ul');
+        const listItem = document.createElement('li');
+        listItem.textContent = quote;
+        list.appendChild(listItem);
+        form.appendChild(list);
 })
-
 
 
