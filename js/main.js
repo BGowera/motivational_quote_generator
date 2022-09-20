50e1a8dc5a42c29bad53723079c42fa63f246476
BGowera <91614476+BGowera@users.noreply.github.com>
2022-09-20 08:11:24 +0200

    changed btn functionality slightly

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 
 const form = document.getElementById('form');
-
-form.addEventListener('submit',async function (e) { 
+const btn = document.getElementById('get-btn');
+form.appendChild(btn);
+form.addEventListener('submit', async function (e) {
         e.preventDefault();
         const req = await axios.get("https://type.fit/api/quotes");
-      
-const randCall = Math.floor(Math.random() * 10) ;
-        const quote = `${req.data[randCall].text}, - ${req.data[randCall].author}`;
+        console.log(req.data)
+const randCall = Math.floor(Math.random() * 1642) ;
+        const quote = `${req.data[randCall].text} - ${req.data[randCall].author}`;
         const list = document.createElement('ul');
         const listItem = document.createElement('li');
+
         listItem.textContent = quote;
         list.appendChild(listItem);
         form.appendChild(list);
+        btn.textContent = "Generate A New Quote";
 })
 
 
