1892f492790a8bd788b6d1dd0aa78d3cb5b433dc
BGowera <91614476+BGowera@users.noreply.github.com>
2022-09-22 13:41:26 +0200

    stopped div stacking+more basic functionality

--- js/main.js
+++ js/main.js
@@ -1,26 +1,55 @@
-const form = document.getElementById('form');
-const btn = document.getElementById('get-btn');
-const quoteDiv = document.querySelector('.quote-div');
+const container = document.querySelector("#container");
+const form = document.getElementById("form");
+const btn = document.getElementById("get-btn");
+let quoteDiv;
+let newBtn;
 form.appendChild(btn);
+container.appendChild(form);
 
-form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        newQuote();
+const newQuote = async () => {
+	const result = await axios.get("https://type.fit/api/quotes");
+	const randomQuote = Math.floor(Math.random() * 1642);
+	const quote = result.data[randomQuote].text;
+	const attribution = result.data[randomQuote].author;
+	quoteDiv = document.createElement("div");
+	const quotePara = document.createElement("p");
+	const attributionPara = document.createElement("p");
+	quotePara.textContent = quote;
+	attributionPara.textContent = `- ${attribution}`;
+	quoteDiv.classList.add("quote-div");
+	quotePara.classList.add(".quote-para");
+	attributionPara.classList.add(".attribution-para");
+	quoteDiv.appendChild(quotePara);
+	quoteDiv.appendChild(attributionPara);
+	container.appendChild(quoteDiv);
+};
+
+const clear = () => {
+	container.removeChild(quoteDiv);
+};
+
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	
+});
+
+
+btn.addEventListener('click', async () => {
+       await newQuote();
+        form.removeChild(btn);
+        newBtn = document.createElement('button');
+        newBtn.innerText = "Get Another Quote";
+        form.appendChild(newBtn);
+
+        newBtn.addEventListener('click', () => {
+                clear();
+                newQuote();
+        })
 })
 
 
-//   1642 quotes total
 
 
-const newQuote = async () => {
-        const result = await axios.get("https://type.fit/api/quotes");
-        const randomQuote = Math.floor(Math.random() * 1642);
-        const quote = result.data[randomQuote].text;
-        const attribution = result.data[randomQuote].author;
-        const quotePara = document.querySelector('.quote');
-        quotePara.textContent = quote;
-        const attributionPara = document.querySelector('.attribution');
-        attributionPara.textContent = `\t -${attribution}`;
-}
 
 
