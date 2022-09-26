f2b7b1c86765dbe4e1149bf1fd3017101779c2cf
BGowera <91614476+BGowera@users.noreply.github.com>
2022-09-26 15:21:57 +0200

    gui fixes and logic side fixes

--- js/main.js
+++ js/main.js
@@ -1,61 +1,81 @@
 const container = document.querySelector("#container");
-const welcomeSection = document.getElementById('welcome-section');
+const welcomeSection = document.getElementById("welcome-section");
 const quoteSection = document.getElementById("quote-section");
 const form = document.getElementById("form");
 const btn = document.getElementById("get-btn");
 let quoteDiv;
+let quotePara;
+let attributionPara;
 let newBtn;
+let result;
 form.appendChild(btn);
 quoteSection.appendChild(form);
 container.appendChild(welcomeSection);
 container.appendChild(quoteSection);
 
+const images = [];
 
-const newQuote = async () => {
-	const result = await axios.get("https://type.fit/api/quotes");
+for (let i = 1; i <= 12; i++) {
+	images.push(`${i}.jpg`);
+}
+function bgRandomize() {
+	const randPic = Math.floor(Math.random() * images.length);
+	return randPic;
+}
+
+function domManip() {
 	const randomQuote = Math.floor(Math.random() * 1642);
 	const quote = result.data[randomQuote].text;
 	const attribution = result.data[randomQuote].author;
 	quoteDiv = document.createElement("div");
-	const quotePara = document.createElement("p");
-	const attributionPara = document.createElement("p");
+	quotePara = document.createElement("p");
+	attributionPara = document.createElement("p");
 	quotePara.textContent = quote;
-	attributionPara.textContent = `- ${attribution}`;
+
+	if (result.data[randomQuote].author == null) {
+		attributionPara.textContent = "- Anonymous";
+	} else {
+		attributionPara.textContent = `- ${attribution}`;
+	}
+	
 	quoteDiv.classList.add("quote-div");
+	quoteDiv.style.backgroundImage = `url(/img/${bgRandomize()}.jpg)`;
 	quotePara.classList.add("quote-para");
 	attributionPara.classList.add("attribution-para");
 	quoteDiv.appendChild(quotePara);
-    quoteDiv.appendChild(attributionPara);
-    quoteSection.appendChild(quoteDiv);
-    
+	quoteDiv.appendChild(attributionPara);
+	quoteSection.appendChild(quoteDiv);
+}
+
+const newQuote = async () => {
+	result = await axios.get("https://type.fit/api/quotes");
+	console.log(result.data);
+	domManip();
 };
 
 const clear = () => {
 	quoteSection.removeChild(quoteDiv);
 };
 
-
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
-	
 });
 
+try {
+	btn.addEventListener("click", async () => {
+		await newQuote();
+		form.removeChild(btn);
 
-btn.addEventListener('click', async () => {
-       await newQuote();
-        form.removeChild(btn);
-
-	newBtn = document.createElement('button');
-        newBtn.innerText = "Get Another Quote";
-        form.appendChild(newBtn);
-        newBtn.addEventListener('click', () => {
-                clear();
+		newBtn = document.createElement("button");
+		newBtn.innerText = "Get Another Quote";
+		form.appendChild(newBtn);
+		newBtn.addEventListener("click", () => {
+			clear();
 			newQuote();
-			newBtn.classList.add('clicked');
-        })
-})
-
-
-
-
-
+			newBtn.classList.add("clicked");
+		});
+	});
+} catch (e) {
+	console.log("oops");
+	container.removeChild(form);
+}
